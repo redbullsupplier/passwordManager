a20a0796c18240573e60c30097d7543be94f641e
RedBullSupplier <alghofaily2005@gmail.com>
2026-08-17 02:12:34 +0300

    I FIXED FLAWS, SO MANY FLAWS, UI OVERHAULED

--- gui.py
+++ gui.py
@@ -39,27 +39,27 @@ def run_app():
 def show_add_entry_screen(root):
     # field 1
     master_password_entry = ttk.Entry(root, show="*")
-    master_password_label = ttk.Label(root, text="Insert your master password.")
-    master_password_label.pack()
-    master_password_entry.pack(pady=10, padx=20)
+    master_password_label = ttk.Label(root, text="Insert your master password:")
+    master_password_label.place(relx= 0.2, rely=0.4, anchor="center")
+    master_password_entry.place(relx=0.6, rely=0.4, relwidth=0.5, anchor="center")
 
     # field 2
     service_name_entry = ttk.Entry(root)
-    service_name_label = ttk.Label(root, text = "Service name")
-    service_name_label.pack()
-    service_name_entry.pack(pady=10, padx=20)
+    service_name_label = ttk.Label(root, text = "Service name: ")
+    service_name_label.place(relx=0.2, rely=0.47, anchor="center")
+    service_name_entry.place(relx=0.6, rely=0.47, relwidth=0.5, anchor="center")
 
     # field 3
     user_email_entry = ttk.Entry(root)
-    user_email_label = ttk.Label(root, text="Please insert the username/email.")
-    user_email_label.pack()
-    user_email_entry.pack(pady=10, padx=20)
+    user_email_label = ttk.Label(root, text="Please insert the username/email: ")
+    user_email_label.place(relx=0.05, rely=0.54, anchor="w")
+    user_email_entry.place(relx=0.6, rely=0.54, relwidth=0.5, anchor="center")
 
     # field 4
     password_entry = ttk.Entry(root, show="*")
-    password_label = ttk.Label(root, text="Please insert the password.")
-    password_label.pack()
-    password_entry.pack(pady=10, padx=20)
+    password_label = ttk.Label(root, text="Please insert the password: ")
+    password_label.place(relx=0.08, rely=0.61, anchor="w")
+    password_entry.place(relx=0.6, rely=0.61, relwidth=0.5, anchor="center")
 
 
 
@@ -74,9 +74,9 @@ def show_add_entry_screen(root):
         clear_screen(root)
         show_vault_screen(root)
 
-    # button 1
+    #button 1
     button_for_entry = ttk.Button(root, text="Submit", command=submit_entry)
-    button_for_entry.pack(pady=10, padx=20)
+    button_for_entry.place(relx=0.8, rely=0.7, anchor="center")
 
     def generate_pass():
         randnum = secrets.SystemRandom().randint(8, 18)
@@ -84,9 +84,9 @@ def show_add_entry_screen(root):
         password_entry.delete(0, tk.END)
         password_entry.insert(0, randompass)
 
-    # button 2
+    #button 2
     button_for_generation = ttk.Button(root, text="Generate Password", command=generate_pass)
-    button_for_generation.pack()
+    button_for_generation.place(relx=0.2, rely=0.7, anchor="center")
 
 def show_vault_screen(root):
     def populate_listbox(entries):
@@ -102,17 +102,17 @@ def show_vault_screen(root):
     ids = []
     populate_listbox(entries)
 
-    list_funsies.pack()
+    list_funsies.place(relx=0.28, rely=0.1, relwidth=0.5, relheight=0.7, anchor="n")
     def on_entry_click(event):
         idHelper = list_funsies.curselection()
         logical = logic.retrieve_entry(current_master_password, ids[idHelper[0]])
         popup = tk.Toplevel(root)
         user_email_entry = ttk.Entry(popup)
-        popup.geometry("200x200")
-        user_email_entry.pack()
+        popup.geometry("400x100")
+        user_email_entry.place(relx=0.45, rely=0.1, relwidth=0.8, anchor="n")
         user_email_entry.insert(0, logical['user_or_email'])
         password_entry = ttk.Entry(popup)
-        password_entry.pack()
+        password_entry.place(relx=0.45, rely=0.4, relwidth=0.8, anchor="n")
         password_entry.insert(0, logical['password'])
     list_funsies.bind("<<ListboxSelect>>", on_entry_click)
 
@@ -123,14 +123,14 @@ def show_vault_screen(root):
         show_vault_screen(root)
 
     deleteButton = ttk.Button(root, text="Delete", command=delete_entry)
-    deleteButton.pack()
+    deleteButton.place(relx=0.28, rely=0.85, anchor="center")
 
     def add_another_entry():
         clear_screen(root)
         show_new_entry_screen(root)
-
+    
     addButton = ttk.Button(root, text = "Add another entry", command=add_another_entry)
-    addButton.pack()
+    addButton.place(relx=0.13, rely=0.85, anchor="center")
 
     def run_search():
         search_term = search_entry.get()
@@ -138,13 +138,13 @@ def show_vault_screen(root):
         populate_listbox(results)
 
     search_label = ttk.Label(root, text="Search for a service name:")
-    search_label.pack()
+    search_label.place(relx=0.315, rely=0.05, anchor="w")
     search_entry = ttk.Entry(root)
-    search_entry.pack()
+    search_entry.place(relx=0.7, rely=0.05, relwidth=0.3, anchor="center")
 
 
     searchButton = ttk.Button(root, text="Search", command=run_search)
-    searchButton.pack()
+    searchButton.place(relx=0.9, rely=0.05, anchor="center")
 
     
 
@@ -152,11 +152,10 @@ def show_vault_screen(root):
 
 def returning_user_screen(root):
     master_password_entry = ttk.Entry(root, show="*")
-    master_password_label = ttk.Label(root, text="Insert your master password.")
-    master_password_label.pack()
-    master_password_entry.pack(pady=10, padx=20)
-    failure_label = ttk.Label(root)
-    failure_label.pack()
+    master_password_label = ttk.Label(root, text="Insert your master password: ")
+    master_password_label.place(relx=0.1, rely=0.4, anchor="w")
+    master_password_entry.place(relx=0.6, rely=0.4, relwidth=0.3, anchor="center")
+
 
     def comparison():
         compare = logic.request_master_password(master_password_entry.get())
@@ -166,14 +165,12 @@ def returning_user_screen(root):
             clear_screen(root)
             show_vault_screen(root)
         else:
-            failure_label.config(text="Wrong master password.")
+            master_password_label.config(text="Wrong master password.")
 
     master_password_button = ttk.Button(root, text="Submit", command=comparison)
-    master_password_button.pack()
+    master_password_button.place(relx=0.82, rely=0.4, relwidth=0.1, anchor="center")
 
-        
-    
-        
+
 def clear_screen(root):
     for widget in root.winfo_children():
         widget.destroy()
@@ -182,21 +179,21 @@ def clear_screen(root):
 def show_new_entry_screen(root):
     # field 1
     service_name_entry = ttk.Entry(root)
-    service_name_label = ttk.Label(root, text = "Service name")
-    service_name_label.pack()
-    service_name_entry.pack(pady=10, padx=20)
+    service_name_label = ttk.Label(root, text = "Service name: ")
+    service_name_label.place(relx=0.2, rely=0.47, anchor="center")
+    service_name_entry.place(relx=0.6, rely=0.47, relwidth=0.5, anchor="center")
 
     # field 2
     user_email_entry = ttk.Entry(root)
-    user_email_label = ttk.Label(root, text="Please insert the username/email.")
-    user_email_label.pack()
-    user_email_entry.pack(pady=10, padx=20)
+    user_email_label = ttk.Label(root, text="Please insert the username/email: ")
+    user_email_label.place(relx=0.05, rely=0.54, anchor="w")
+    user_email_entry.place(relx=0.6, rely=0.54, relwidth=0.5, anchor="center")
 
     # field 3
     password_entry = ttk.Entry(root, show="*")
-    password_label = ttk.Label(root, text="Please insert the password.")
-    password_label.pack()
-    password_entry.pack(pady=10, padx=20)
+    password_label = ttk.Label(root, text="Please insert the password: ")
+    password_label.place(relx=0.08, rely=0.61, anchor="w")
+    password_entry.place(relx=0.6, rely=0.61, relwidth=0.5, anchor="center")
 
     def submit_entry():
         the_service_name = service_name_entry.get()
@@ -206,9 +203,9 @@ def show_new_entry_screen(root):
         clear_screen(root)
         show_vault_screen(root)
 
-    # button 1
+        #button 1
     button_for_entry = ttk.Button(root, text="Submit", command=submit_entry)
-    button_for_entry.pack(pady=10, padx=20)
+    button_for_entry.place(relx=0.8, rely=0.7, anchor="center")
 
     def generate_pass():
         randnum = secrets.SystemRandom().randint(8, 18)
@@ -216,8 +213,8 @@ def show_new_entry_screen(root):
         password_entry.delete(0, tk.END)
         password_entry.insert(0, randompass)
 
-    # button 2
+    
     button_for_generation = ttk.Button(root, text="Generate Password", command=generate_pass)
-    button_for_generation.pack()
+    button_for_generation.place(relx=0.2, rely=0.7, anchor="center")
 
     
--- logic.py
+++ logic.py
@@ -7,9 +7,9 @@ def request_master_password(master_password):
     first_entry = all_data[0]
     first_entry_id = first_entry[1]
     attempt = storage.lookup_id(first_entry_id) # encrypted_data, salt
-    #key = crypto.derive_key(master_password, attempt[1]) dead code! more dead code! KILL
+    key = crypto.derive_key(master_password, attempt[1]) #im literally a moron this was needed the entire time how did i forget im so dumb
     try:
-         #decrypted = crypto.decrypt_entry(attempt[0], key) dead code lol
+         decrypted = crypto.decrypt_entry(attempt[0], key) 
          return True
     except InvalidToken:
          return False
